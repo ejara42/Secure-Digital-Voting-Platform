96218c3e9c49064e978fb61bf2d77cdfcc3870e3
ejara <ejarawo@gmail.com>
2026-03-28 15:09:07 +0300

    Allow CORS traffic from Vercel

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,7 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
+    "https://secure-digital-voting-platform.vercel.app",
     process.env.FRONTEND_URL,
 ].filter(Boolean);
 
